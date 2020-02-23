52d3d14987e9dd6e06202f6b8bb8fee129ef2ac5
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-02-23 11:00:31 +0100

    added watch

--- .idea/index.js
+++ .idea/index.js
@@ -3,8 +3,11 @@ const led = new Gpio(17, 'out');
 const button = new Gpio(4, 'in', 'both');
 
 button.watch(
-    (err, value) => led.writeSync(value),
-    console.log('button pressed')
+    (err, value) => led.writeSync(value)
+);
+
+button.watch(
+    (err, value) => console.log('led blinked')
 );
 
 console.log('End of node file');
