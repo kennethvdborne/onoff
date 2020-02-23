6ea372f28e2ce602fdaa620949601fac5e70d6ac
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-02-23 10:43:04 +0100

    added log

--- .idea/index.js
+++ .idea/index.js
@@ -2,4 +2,9 @@ const Gpio = require('onoff').Gpio;
 const led = new Gpio(17, 'out');
 const button = new Gpio(4, 'in', 'both');
 
-button.watch((err, value) => led.writeSync(value));
+button.watch(
+    (err, value) => led.writeSync(value),
+    console.log('button pressed')
+);
+
+console.log('End of node file');
