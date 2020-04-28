d92315a99712fc32f5babeaae8befb7681797df8
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-04-28 19:21:39 +0200

    testing

--- .idea/src/classes/httpHelper.js
+++ .idea/src/classes/httpHelper.js
@@ -1,12 +1,28 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-function http() {
-    var request = new XMLHttpRequest();
 
-    request.open('GET', url + '/api/playscene/0eb672ee-b7f8-485c-bcb9-774497f5f0b7', true);
-    request.onload = function() {
-        console.log("worked............");
+class HttpHelper {
+
+    constructor(url) {
+        this.url = url;
+        XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     }
-    console.log("busy...........")
 
-    request.send();
+    //TODO record,play,pause,...
+
+    method() {
+        request.open('GET', url + '/api/playscene/0eb672ee-b7f8-485c-bcb9-774497f5f0b7', true);
+        request.onload = function() {
+            console.log("worked............");
+        }
+        console.log("busy...........")
+
+        request.send();
+    }
+
+
+
 }
+
+var a = new HttpHelper();
+
+module.exports = {HttpHelper: HttpHelper};
