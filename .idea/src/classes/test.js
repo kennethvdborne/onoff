4afc5df553f9a31074440ba2de5221e1d8371969
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-04-28 20:10:12 +0200

    testing

--- .idea/src/classes/test.js
+++ .idea/src/classes/test.js
@@ -5,6 +5,7 @@ module.exports = {
     http : function(req) {
 
         var request = new XMLHttpRequest();
+        console.log('testetetetetette')
 
         request.open('GET', 'http://localhost:8080/' + '/api/playscene/0eb672ee-b7f8-485c-bcb9-774497f5f0b7', true);
         request.onload = function () {
