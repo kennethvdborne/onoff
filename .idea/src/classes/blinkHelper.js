27dcf928a317de056e45080ad2b924d98fb0eddb
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-04-29 17:45:59 +0200

    testing

--- .idea/src/classes/blinkHelper.js
+++ .idea/src/classes/blinkHelper.js
@@ -3,17 +3,17 @@ var ledx;
 var blinkInterval = setInterval(blinkStart(), 300);
 
 function blinkStart() {
-    if (this.ledx.readSync() === 0) {
-        this.ledx.writeSync(1);
+    if (ledx.readSync() === 0) {
+        ledx.writeSync(1);
     } else {
-        this.ledx.writeSync(0);
+        ledx.writeSync(0);
     }
 }
 
 function blinkEnd() {
     clearInterval(blinkInterval);
-    this.ledx.writeSync(0);
-    this.ledx.unexport();
+    ledx.writeSync(0);
+    ledx.unexport();
 }
 
 module.exports.blinkStart = blinkStart;
