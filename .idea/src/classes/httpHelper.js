71b28120f8a57f7100411dd698da2b13339be159
matthiassomay <matthiassomay@gmail.com>
2020-04-29 12:18:15 +0200

    http

---
+++ .idea/src/classes/httpHelper.js
@@ -0,0 +1,32 @@
+const url = 'http://localhost:8080/';
+const fetch = require('node-fetch');
+
+module.exports = {
+
+    recordScene : function(button) {
+
+        fetch(url + '/api/recordscenebis/' + button);
+        
+    },
+
+    playScene : function (button) {
+
+        fetch(url + '/api/playscene/' + button);
+    },
+
+    getButtonsWithScene: async function() {
+
+        let obj;
+
+        var response = await fetch(url + 'api/getbuttonswithscene')
+            //.then(res => res.json())
+            //.then(json => obj = json)
+            //.then(json => console.log(json));
+        
+        obj = await response.text();
+        console.log(obj);
+
+        
+
+    }
+}
---
+++ .idea/src/classes/init.js
@@ -0,0 +1,4 @@
+const http = require('./httpHelper');
+http.getButtonsWithScene();
+http.playScene("0eb672ee-b7f8-485c-bcb9-774497f5f0b7");
+
