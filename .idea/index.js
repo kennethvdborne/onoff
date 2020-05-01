294af30ff765a5619c11c4f263c97a84899c70df
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-05-01 16:29:07 +0200

    testing

--- .idea/index.js
+++ .idea/index.js
@@ -86,7 +86,7 @@ button2.watch((err, value) => {
         throw err;
     }
     if (sys2 && value === 1) {
-        blinkHelper.blinkSetLeds(null);
+        blinkHelper.setLeds(null);
         sys2 = false;
         setTimeout(function(){
             sys2 = true;
