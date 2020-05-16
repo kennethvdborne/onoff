3f48e5b5a408145dd3056c7426f6441a24cc1946
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-05-16 15:15:57 +0200

    testing

--- .idea/index.js
+++ .idea/index.js
@@ -63,7 +63,10 @@ function buttonFunctions(led, x) {
         httpHelper.recordScene(led, x, ledsFunction, ledRecord);
         ledRecord.writeSync(1);
     }
-    if (playMode, x) {
+    if (playMode) {
+        console.log(x);
+        var y = x+1;
+        console.log(y);
         if (buttonsInUse[x+1]) {
             console.log('button has a scene');
             blinkHelper.blinkEnd(ledPlay);
