8e47490f352b7afd92f94180b88d7cf36e5fdfbc
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-05-17 16:23:39 +0200

    testing

--- .idea/src/classes/httpHelper.js
+++ .idea/src/classes/httpHelper.js
@@ -3,75 +3,78 @@ const fetch = require('node-fetch');
 const blinkHelper = require('./blinkHelper');
 const main = require('../../index');
 
-module.exports = {
+function recordScene(led, button, ledRecord) {
 
-    recordScene : function(led, button, ledRecord) {
+    console.log('recordfromhhhtp');
 
-        console.log('recordfromhhhtp');
-
-        (fetch(url + '/api/recordSceneSingleFrame/' + button))
-            .then(res => res.text())
-            .then(function (res) {
-                if (res == 'true'){
-                    blinkHelper.blinkConfirm(led);
-                }
-                else {
-                    blinkHelper.blinkStart(ledRecord);
-                }
-            })
-    },
-
-    recordSceneMultiple : function(led, button, ledRecord) {
-
-        console.log('recordfromhhhtp');
-
-        (fetch(url + '/api/recordSceneMultipleFrames/' + button))
-            .then(res => res.text())
-            .then(function (res) {
-                if (res == 'true'){
-                    console.log('rec started');
-                    led.writeSync(1);
-                }
-                else {
-                    blinkHelper.blinkStart(ledRecord);
-                }
-            })
-    },
-
-    stopRecording : function(led, ledRecord) {
-
-        console.log('stoprecording');
-
-        (fetch(url + '/api/stopRecording'))
-            .then(res => res.text())
-            .then(function (res) {
-                if (res == 'true'){
-                    console.log('true recording');
-                    led.writeSync(0);
-                    blinkHelper.blinkConfirm(led);
-                }
-                else {
-                    console.log('false recording');
-                    blinkHelper.blinkFastStart(ledRecord);
-                }
-            })
-    },
-
-    playScene : function (button) {
-        fetch(url + '/api/playscene/' + button);
-    },
-
-    getButtons: function(leds, mode){
-        fetch(url + 'api/getbuttons')
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(json){
-                blinkHelper.blinkSpecificLedsStart(json, leds, mode);
-                main.setButtonsInUse(json);
-            });
-    },
-
-
-
+    (fetch(url + '/api/recordSceneSingleFrame/' + button))
+        .then(res => res.text())
+        .then(function (res) {
+            if (res == 'true'){
+                blinkHelper.blinkConfirm(led);
+            }
+            else {
+                blinkHelper.blinkStart(ledRecord);
+            }
+        })
 }
+
+function recordSceneMultiple(led, button, ledRecord) {
+
+    console.log('recordfromhhhtp');
+
+    (fetch(url + '/api/recordSceneMultipleFrames/' + button))
+        .then(res => res.text())
+        .then(function (res) {
+            if (res == 'true'){
+                console.log('rec started');
+                led.writeSync(1);
+            }
+            else {
+                blinkHelper.blinkStart(ledRecord);
+                getButtons(ledsFunction, 'Record');
+            }
+        })
+}
+
+function stopRecording(led, ledRecord) {
+
+    console.log('stoprecording');
+
+    (fetch(url + '/api/stopRecording'))
+        .then(res => res.text())
+        .then(function (res) {
+            if (res == 'true'){
+                console.log('true recording');
+                led.writeSync(0);
+                blinkHelper.blinkConfirm(led);
+            }
+            else {
+                console.log('false recording');
+                blinkHelper.blinkFastStart(ledRecord);
+                getButtons(ledsFunction, 'Record');
+            }
+        })
+}
+
+function playScene(button) {
+    fetch(url + '/api/playscene/' + button);
+}
+
+function getButtons(leds, mode){
+    fetch(url + 'api/getbuttons')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            blinkHelper.blinkSpecificLedsStart(json, leds, mode);
+            main.setButtonsInUse(json);
+        });
+}
+
+module.exports.recordScene = recordScene;
+module.exports.recordSceneMultiple = recordSceneMultiple;
+module.exports.stopRecording = stopRecording;
+module.exports.playScene = playScene;
+module.exports.getButtons = getButtons;
+
